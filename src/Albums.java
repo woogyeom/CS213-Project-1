bf90741a8d62726c06eae8901465da6beee2abf1
Aravind <aravindchundu@gmail.com>
2024-02-05 18:15:06 -0500

    Added all the classes mentioned in the project1 writeup. Also renamed main.java to RunProject1.java

--- src/Albums.java
+++ src/Albums.java
@@ -1,2 +1,14 @@
-public class Albums {
+public class Album {
+    private String title;
+    private Artist artist;
+    private Genre genre;
+    private Date released;
+    private Rating ratings; //a linked list of ratings
+
+    public void rate(int star) {
+
+    } //add a rating to the linked list
+    public double avgRatings() {
+
+    } //compute the average ratings
 }
